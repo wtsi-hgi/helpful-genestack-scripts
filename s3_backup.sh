fe4201028dcfa097fa8c57aff2ad5052493efe19
michael-grace <michael.dp.grace@outlook.com>
2022-04-01 17:37:09 +0100

    some job submission fixes

--- s3_backup.sh
+++ s3_backup.sh
@@ -2,8 +2,8 @@
 
 # Author: Michael Grace <mg38@sanger.ac.uk>
 
-set -euo pipefail
 source /usr/local/lsf/conf/profile.lsf
+set -euo pipefail
 
 DIR=/lustre/scratch119/humgen/teams/hgi/genestack/backups
 IRODS_LOC=/humgen/teams/hgi/genestack-backups
@@ -15,7 +15,9 @@ log () {
 }
 
 submit_jobs_cpu_count () {
-    bsub -o .tmp/%J -e .tmp/%J -g hgi -n $1 ${@:2} | awk -F '<|>' '{print $2}'
+	new=.tmp/$RANDOM$RANDOM$RANDOM
+	echo ${@:2} > $new
+    bsub -o .tmp/%J -e .tmp/%J -G hgi -n $1 bash $new | awk -F '<|>' '{print $2}'
 }
 
 submit_job () {
@@ -25,13 +27,12 @@ submit_job () {
 tail_job_output() {
     local JOB_ID=$1
 
-    tail -f .tmp/$JOB_ID &
-    local TAIL_ID=$?
-
     while true; do
-        [[ $(bjobs | grep $JOB_ID) == "" ]] && kill $TAIL_ID
-        sleep 5
+	    [[ $(bjobs | grep $JOB_ID) == "" ]] && break
+	    sleep 5
     done
+
+    cat .tmp/$JOB_ID
 }
 
 process_bucket () {
@@ -47,7 +48,7 @@ process_bucket () {
     # Do we make a new tarball?
     if [[ $(echo $RCLONE_OUT | grep '0 Bytes') == "" ]]; then
         log $bucket "Something's different - let's create a new tarball"
-        
+
         # Let's look at what we're not including (anything over 5GB)
         echo Excluding These Files - They are Over 5GB > .s3_backup_$bucket/BACKUP_README
         find .s3_backup_$bucket -size +5G >> .s3_backup_$bucket/BACKUP_README
@@ -55,8 +56,7 @@ process_bucket () {
         local FNAME=$(date -u '+%Y%m%d%H%M%S').$bucket.tar.gz
         
         # Make the original tarball
-        local JOB_ID=$(submit_job find .s3_backup_$bucket -size +5G -exec echo --exclude={} \; | \
-            xargs -I '{}' tar '{}' -czf $FNAME .s3_backup_$bucket)
+        local JOB_ID=$(submit_job find .s3_backup_$bucket -size +5G -exec echo --exclude={} \\\; \| xargs -I '{}' tar '{}' -czf $DIR/$FNAME .s3_backup_$bucket)
 
         log $bucket Tarball Job - $JOB_ID
         tail_job_output $JOB_ID
@@ -68,7 +68,7 @@ process_bucket () {
         # Let's Split It Up
         mkdir .tmp_tar.$bucket
         local JOB_ID=$(submit_job split -b 10G -d $FNAME .tmp_tar.$bucket/$bucket.tar.gz.)
-        
+
         log $bucket Split Job - $JOB_ID
         tail_job_output $JOB_ID
 
@@ -78,7 +78,7 @@ process_bucket () {
 
         log $bucket IRODS Sync Job - $JOB_ID
         tail_job_output $JOB_ID
-        
+
         # Delete Old Files from IRODS
         for irods_file in $(ils $IRODS_LOC/$bucket); do
             [[ -f .tmp_tar.$bucket/$irods_file ]] || irm $IRODS_LOC/$bucket/$irods_file && log $bucket Deleted $irods_file from IRODS
@@ -101,7 +101,7 @@ process_bucket () {
 }
 
 cd $DIR
-mkdir .tmp
+mkdir -p .tmp
 
 log root Making Buckets Public
 gs-public > /dev/null
@@ -112,7 +112,7 @@ done
 
 wait
 
-/bin/rm -r .tmp
+#/bin/rm -r .tmp
 cd - > /dev/null
 
 log root Making Buckets Private
