f060d8e2e90fde8458c35829d05e0c8961068b1c
Guillaume Noell <50482094+gn5@users.noreply.github.com>
2022-05-17 14:29:03 +0100

    update to python3

--- delete-study/delete_study.py
+++ delete-study/delete_study.py
@@ -1,21 +1,16 @@
-#!/usr/bin/env python2.7
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-#
-# Copyright (c) 2011-2020 Genestack Limited
-# All Rights Reserved
-# THIS IS UNPUBLISHED PROPRIETARY SOURCE CODE OF GENESTACK LIMITED
-# The copyright notice above does not evidence any
-# actual or intended publication of such source code.
-#
 
 # This script deletes studies. Provide study accession of a study which needs to be deleted.
 
 # See the full instruction at:
 # https://genestack.atlassian.net/wiki/spaces/ODMP/pages/1225818141/How+to+delete+a+study
+from __future__ import print_function, unicode_literals
+
 import re
 
-from genestack_client import get_connection, make_connection_parser
+from genestack_client import GenestackServerException
+from genestack_client.utils import make_connection_parser, get_connection
 
 from utils import colored, GREEN, RED
 
@@ -31,14 +26,15 @@ def main():
     study_accession = args.study_accession
     try:
         connection.application('genestack/arvados-importer').invoke('wipeStudy', study_accession)
-        print colored('Success', GREEN)
-    except Exception as e:
+        print(colored("Success", GREEN))
+    except GenestackServerException as e:
         p = re.compile('File .* not found')
         result = p.search(e.message)
         if result is not None:
-            print colored('Study with accession %s does not exist' % study_accession, RED)
+            print(colored("Study with accession %s does not exist" % study_accession,
+                          RED))
         else:
-            print colored(e, RED)
+            print(colored(e, RED))
 
 
 if __name__ == "__main__":
