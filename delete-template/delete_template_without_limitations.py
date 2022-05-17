7a5d4afbb182d20580a81a40020dc08fe61eb8d1
Guillaume Noell <50482094+gn5@users.noreply.github.com>
2022-05-17 14:25:50 +0100

    updated script for python3

--- delete-template/delete_template_without_limitations.py
+++ delete-template/delete_template_without_limitations.py
@@ -1,27 +1,21 @@
-#!/usr/bin/env python2.7
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-#
-# Copyright (c) 2011-2020 Genestack Limited
-# All Rights Reserved
-# THIS IS UNPUBLISHED PROPRIETARY SOURCE CODE OF GENESTACK LIMITED
-# The copyright notice above does not evidence any
-# actual or intended publication of such source code.
-#
-
 # This script deletes templates. Provide accession of a template which needs to be deleted.
-# This script is a temprorary solution and may damage studies and delete default template.
+# This script is a temporary solution and may damage studies and delete default template.
 
 # Before template deletion all the studies which have this template set should be manually changed:
 # other template which is not going to be deleted should be applied.
 
-# Done in the scope of https://genestack.atlassian.net/browse/BR-106
-
 # See the full instruction at:
 # https://genestack.atlassian.net/wiki/spaces/ODMP/pages/1236926591/How+to+delete+a+template
+from __future__ import print_function, unicode_literals
+
 import re
 
+from genestack_client import GenestackServerException
 from genestack_client.utils import make_connection_parser, get_connection
+
 from utils import colored, GREEN, RED
 
 
@@ -36,14 +30,15 @@ def main():
     template_accession = args.template_accession
     try:
         connection.application('genestack/arvados-importer').invoke('wipeStudy', template_accession)
-        print colored('Success', GREEN)
-    except Exception as e:
+        print(colored("Success", GREEN))
+    except GenestackServerException as e:
         p = re.compile('File .* not found')
         result = p.search(e.message)
         if result is not None:
-            print colored('Template with accession %s does not exist' % template_accession, RED)
+            print(colored("Template with accession %s does not exist" % template_accession,
+                         RED))
         else:
-            print colored(e, RED)
+            print(colored(e, RED))
 
 
 if __name__ == "__main__":
